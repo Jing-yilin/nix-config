7daf1a1176bdc55ebd413ed503069a141f0eeffd
Jing-yilin <yilin.jing.ai@outlook.com>
2025-01-11 01:49:20 +0000

    fix: update conda paths in zsh configuration
    - Changed hardcoded user paths from '/Users/zephyr/anaconda3' to '~/anaconda3' for improved portability.
    - Ensured that the conda initialization script and profile sourcing use the updated path format.

--- zsh.nix
+++ zsh.nix
@@ -184,14 +184,14 @@
       unsetopt BANG_HIST
       
       # Conda 初始化
-      __conda_setup="$(/Users/zephyr/anaconda3/bin/conda 'shell.zsh' 'hook' 2> /dev/null)"
+      __conda_setup="$(~/anaconda3/bin/conda 'shell.zsh' 'hook' 2> /dev/null)"
       if [ $? -eq 0 ]; then
           eval "$__conda_setup"
       else
-          if [ -f "/Users/zephyr/anaconda3/etc/profile.d/conda.sh" ]; then
-              . "/Users/zephyr/anaconda3/etc/profile.d/conda.sh"
+          if [ -f "~/anaconda3/etc/profile.d/conda.sh" ]; then
+              . "~/anaconda3/etc/profile.d/conda.sh"
           else
-              export PATH="/Users/zephyr/anaconda3/bin:$PATH"
+              export PATH="~/anaconda3/bin:$PATH"
           fi
       fi
       unset __conda_setup
